74374ad595ff5db17b77291f3a14fda80445b102
ALKAGroup <info@alkavisuals.nl>
2026-08-19 14:18:43 +0200

    Add ontwerpboek asset manifest

---
+++ technischbouwadvies/ontwerpboek/asset-manifest.js
@@ -0,0 +1,13 @@
+export const pipelineConfig = {
+  version: 'V0.4',
+  edition: 'ASSET PIPELINE TEST'
+};
+
+export const assetManifest = {
+  '06-plattegrond': {
+    src: 'assets/project/06-plattegrond.svg',
+    alt: 'Testplattegrond voor de V0.4 contentpijplijn van Technisch Bouwadvies',
+    fit: 'contain',
+    label: 'Asset pipeline · technische tekening'
+  }
+};
